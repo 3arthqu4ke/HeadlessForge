4897961cfb037ab02cb4d51540fc6c5343d30eb1
3arthqu4ke <hanspeteroliver.mueller@gmail.com>
2021-04-16 09:54:52 +0200

    Bump version, better Messages when disconnected.

--- src/main/java/me/earth/headlessforge/mixin/gui/MixinGuiDisconnected.java
+++ src/main/java/me/earth/headlessforge/mixin/gui/MixinGuiDisconnected.java
@@ -2,13 +2,26 @@ package me.earth.headlessforge.mixin.gui;
 
 import me.earth.headlessforge.inject.ducks.IGuiMessage;
 import net.minecraft.client.gui.GuiDisconnected;
+import net.minecraft.client.gui.GuiUtilRenderComponents;
+import net.minecraft.util.text.ITextComponent;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(GuiDisconnected.class)
 public abstract class MixinGuiDisconnected implements IGuiMessage {
+    @Shadow
+    @Final
+    private String reason;
+
+    @Shadow
+    @Final
+    private ITextComponent message;
+
     @Override
-    @Accessor("reason")
-    public abstract String getMessage();
+    public String getMessage()
+    {
+        return reason + " : " + GuiUtilRenderComponents.removeTextColorsIfConfigured(message.getUnformattedText(), false);
+    }
 
 }
